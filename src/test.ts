420fb541a1f8c710baa5fb696920019e3baba141
fahimahammed <fahimahammed.cse@gmail.com>
2023-11-26 13:22:08 +0600

    test done

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 import secureEncrypt from './index';
 
-const message = "fAhIM AHAMMedFir# siThY@12 9*&|+-/;45";
+const message = "fAhIM AHAMMedFir# siThY@12 9*&|+-/;45 34 73 FaS832###)\}'0f";
 const key = "asdfFF#@9821sakf"
 const encryptedMessage = secureEncrypt.encrypt(message, key);
 console.log("encrypt: ", encryptedMessage);
